6c50e2557e0a7abd8dac3cc62e6ea29293c79e75
Kauã Berman Amorim <77868006+kauabamorim@users.noreply.github.com>
2022-05-16 13:55:55 -0300

    Soma de Vetores

---
+++ lista04/somaVetores.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class somaVetores {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int somatoria = 0;
+        int a[] = new int[20];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            System.out.print("Informe o " + (i + 1) + "° numero: ");
+            a[i] = scanner.nextInt();
+
+            somatoria += a[i];
+        }
+        System.out.println("Soma Elementos:" + somatoria);
+    }
+}
